5803012130ebcec52d63076422b3ac574e9c47cf
Marc Maldonado <marc@telecomelite.com>
2020-03-02 20:30:04 -0500

    I need to get the post working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,18 @@
-var noteData = require('../data/noteData');
-
+var noteData = require("../db/db.json");
+var uniqid = require('uniqid')
 
 module.exports = function(app) {
 
-    app.get("/api/index", function(req,res){
-        res.json(noteData)
+    app.get("/api/notes", function(req,res){
+        res.json(db)
     });
 
     app.post("/api/notes", function(req,res){
-        noteData.push(req.body)
+        const newNoteData = req.body;
+        const id = uniqid()
+        newNoteData.id = id;
+        db.push(newNoteData)
+        res.json(newNoteData)
     });
 
     app.delete("/api/notes", function(req,res){
