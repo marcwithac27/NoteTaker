1336a5cab2dfda4e1e50f94a1c2a3d80f7397a5b
Marc Maldonado <marc@telecomelite.com>
2020-03-02 19:31:42 -0500

    routes are done I think

---
+++ data/noteData.js
@@ -0,0 +1,10 @@
+var noteData = [
+    {
+        noteSubject: "First Note",
+        noteDate: 12/20/2020,
+        notecomments: "This is the first Note comment",
+        id: 1
+    }
+];
+
+module.exports = noteData;
